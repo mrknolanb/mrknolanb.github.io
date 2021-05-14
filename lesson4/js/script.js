a75914502b7ac6e794b5d3d9ff95f6dfc95d920c
mrknolanb <marknolanb.bullag@gmail.com>
2021-05-15 05:14:23 +0800

    week 4

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -2,3 +2,6 @@ var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' 
 var today  = new Date();
 document.querySelector('#lastmod').textContent = today.toLocaleDateString("en-GB", options);
 document.querySelector('#thisyear').textContent = new Date().getFullYear();
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
