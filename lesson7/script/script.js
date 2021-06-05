4f0c5f9f1384b4d571cc42c366b6f4a36bba3f74
mrknolanb <marknolanb.bullag@gmail.com>
2021-06-05 08:27:18 +0800

    week 7

--- lesson7/script/script.js
+++ lesson7/script/script.js
@@ -10,3 +10,28 @@ if (today.getDay() == 5) {
     document.querySelector('.banner').style.display = "block"
 
 }
+
+/**********Local Storage*********/
+
+function getNumberOfDays(start, end) {
+    const date1 = start;
+    const date2 = end;
+    // One day in milliseconds
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    // Calculating the time difference between two dates
+    const diffInTime = date2 - date1;
+    // Calculating the no. of days between two dates
+    const diffInDays = Math.round(diffInTime / oneDay);
+
+    return diffInDays;
+} 
+
+
+
+let newVisit = Date.now() ;
+let lastVisit = localStorage.getItem("lastVisit") ;
+document.getElementById("result").innerHTML = "You visited this page " + getNumberOfDays(lastVisit, newVisit) + " day/s ago!";
+
+
+localStorage.setItem("lastVisit", newVisit) ;
