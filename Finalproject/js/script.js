d81cd577eb9b6da0b88c1d59aefe8529d2e823d0
mrknolanb <marknolanb.bullag@gmail.com>
2021-07-15 19:42:40 +0800

    project

---
+++ Finalproject/js/script.js
@@ -0,0 +1,83 @@
+function toggleMenu() {
+}
+
+document.querySelector('#thisyear').textContent = new Date().getFullYear();
+
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+    menuOpen = false;
+  }
+});
+
+
+const APIKey = "5976fad0960c50bf09e8fe2e09d89de8"
+const lon = "122.95"
+const lat = "10.66667"
+
+const APIUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`
+fetch(APIUrl)
+.then((response) => response.json())
+.then((jsObject) => {
+  console.log(jsObject);
+  const temperature = document.querySelector('#currentTemp');
+  temperature.textContent = Math.round(jsObject.current.temp) ;
+  const currently = document.querySelector('#currently');
+    const desc = jsObject.current.weather[0].description;
+    currently.innerHTML = `${desc}`;
+    document.getElementById("currently").style.textTransform = "capitalize";
+    const humid = document.querySelector('#humidity');
+    humid.textContent = jsObject.current.humidity
+    const imgsrc = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${jsObject.current.weather[0].icon}.svg`
+    document.getElementById(`iconTemp`).textContent = imgsrc
+    document.getElementById(`iconTemp`).src = imgsrc
+})
+
+const newsAPIKey = "c4666cbb752058d83b3a380d94f770b3"
+
+
+const NewsAPIUrl = `http://api.mediastack.com/v1/news?access_key=${newsAPIKey}&categories=business&countries=ph&sort=popularity`
+fetch(NewsAPIUrl)
+.then((response) => response.json())
+.then((newsJsObject) => {
+  console.log(newsJsObject);
+
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+ 
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+ 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+ 
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+ 
