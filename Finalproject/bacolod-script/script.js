815fa80eeffe4fb16859f945afe41e99d8326ea2
mrknolanb <marknolanb.bullag@gmail.com>
2021-07-22 12:36:57 +0800

    final

--- Finalproject/bacolod-script/script.js
+++ Finalproject/bacolod-script/script.js
@@ -2,6 +2,9 @@ var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' 
 var today  = new Date();
 document.querySelector('#lastmod').textContent = today.toLocaleDateString("en-GB", options);
 
+
+document.querySelector('#thisyear').textContent = new Date().getFullYear();
+
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
